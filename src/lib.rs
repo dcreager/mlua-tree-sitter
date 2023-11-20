61d52bac8d6406f21eee70c94b7e3f23fc817438
Douglas Creager <dcreager@dcreager.net>
2023-11-20 10:36:55 -0500

    Add rustdoc documentation

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,72 @@
 // Please see the LICENSE file in this distribution for license details.
 // ------------------------------------------------------------------------------------------------
 
+//! Provides access to the [`ltreesitter`][ltreesitter] Lua tree-sitter bindings in Lua
+//! environments managed by the [`mlua`][mlua] crate.
+//!
+//! [ltreesitter]: https://github.com/euclidianAce/ltreesitter/
+//! [mlua]: https://docs.rs/mlua/
+//!
+//! ## Parsing from Rust, consuming the parse tree from Lua
+//!
+//! A common use case is to parse source code in Rust using tree-sitter, and then invoke a Lua
+//! script to consume that parse tree:
+//!
+//! ```
+//! # fn main() -> Result<(), anyhow::Error> {
+//! use mlua_tree_sitter::Module;
+//! use mlua_tree_sitter::WithSource;
+//!
+//! // Parse some Python source code
+//! let code = br#"
+//!     def double(x):
+//!         return x * 2
+//! "#;
+//! let mut parser = tree_sitter::Parser::new();
+//! parser.set_language(tree_sitter_python::language())?;
+//! let parsed = parser.parse(code, None).expect("Could not parse Python code");
+//!
+//! // Create a Lua interpreter and define a Lua function that will
+//! // parse the syntax tree.
+//! let lua = mlua::Lua::new();
+//! lua.open_ltreesitter()?;
+//! let chunk = r#"
+//!     function process_tree(parsed)
+//!       local root = parsed:root()
+//!       assert(root:type() == "module", "expected module as root of tree")
+//!     end
+//! "#;
+//! lua.load(chunk).set_name("Lua chunk").exec()?;
+//! let process_tree: mlua::Function = lua.globals().get("process_tree")?;
+//!
+//! // Execute the Lua function
+//! process_tree.call(parsed.with_source(code))?;
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! ## Building
+//!
+//! This crate depends on the [`mlua`][mlua] crate, which supports multiple Lua versions, and can
+//! either link against a system-installed copy of Lua, or build its own copy from vendored Lua
+//! source.  These choices are all controlled via `mlua` features.
+//!
+//! When building and testing this crate, make sure to provide all necessary features on the
+//! command line:
+//!
+//! ``` console
+//! $ cargo test --features mlua/lua54,mlua/vendored
+//! ```
+//!
+//! When building a crate that depends on this crate, add a dependency on `mlua` so that you can
+//! set its feature flags:
+//!
+//! ``` toml
+//! [dependencies]
+//! mlua = { version="0.9", features=["lua54", "vendored"] }
+//! mlua-tree-sitter = { version="0.1" }
+//! ```
+
 use std::ffi::c_char;
 use std::ffi::c_void;
 
